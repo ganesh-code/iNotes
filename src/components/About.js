32f4e920e532a677ea8caece37b6f131c8335749
Ganesh <ganeshmuniganti27@gmail.com>
2023-09-02 02:30:50 +0530

    updated about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,42 @@
-import React  from 'react'
+import React from 'react'
 
 export default function About() {
 
 
   return (
-    <div className='container' style={{position : "absolute", right:0, width:'80vw', height:'100vh'}}>
-      <h1>this is about of  and his age is</h1>
+    <div className='container'>
+      <h1 className='my-4'>About</h1>
+      <h3 className='my-5'>MERN (MongoDB, Express.js, React, Node.js) stack application</h3>
+      <p>Building a MERN (MongoDB, Express.js, React, Node.js) stack application for storing notes in the cloud is a great way to learn how to create a full-stack web application. Below, I'll outline the steps and components you'll need to create such an application:
+
+        <h4 className="my-3">1. Project Setup:</h4>
+
+        <p>Create a new directory for your project and set up a new Node.js project using npm or yarn.
+          - Initialize a Git repository to track your project's code.</p>
+
+        <h4 className="my-5">2. Backend (Server-Side):</h4>
+
+        <p>- Node.js and Express.js: Set up a Node.js server using Express.js as your backend framework.</p>
+        <p> - MongoDB: Create a MongoDB database to store note data.</p>
+        <p> - Mongoose: Use Mongoose to define data models and interact with the MongoDB database.</p>
+        <p> - API Routes: Create API routes for CRUD (Create, Read, Update, Delete) operations on notes.</p>
+        <p>- User Authentication: Implement user authentication using packages like Passport.js or JWT (JSON Web Tokens).</p>
+
+        <h4 className="my-5">  3. Frontend (Client-Side):</h4>
+        <p> - React.js: Set up a React.js application as the frontend.</p>
+        <p> - React Router: Implement client-side routing using React Router.</p>
+        <p> - User Interface: Design and build user interfaces for viewing, creating, updating, and deleting notes.</p>
+        <p>- Forms: Create forms for adding and editing notes.</p>
+        <p> - Context or Redux: Use React Context API or Redux for state management.</p>
+        <p> - Authentication Flow: Implement user registration and login features.</p>
+        <p>- API Requests: Make API requests to interact with the backend for CRUD operations.</p>
+        <h4 className="my-5"> 4. User Features:</h4>
+        <p>- User Registration: Allow users to create accounts with email and password.</p>
+        <p>- User Login: Enable users to log in to their accounts securely.</p>
+        <p>- User Profile: Create a user profile page displaying user information.</p>
+        <p> - Password Reset: Implement a password reset feature with email verification.</p>
+        <p>- Authentication Middleware: Protect routes that require authentication.</p>
+        Building a MERN stack notes app with cloud storage is a comprehensive project that covers various aspects of web development, including backend and frontend development, authentication, cloud integration, and more. It's a valuable learning experience for developers looking to enhance their full-stack development skills.</p>
     </div>
   )
 }
--- src/components/AddNotesBtn.js
+++ src/components/AddNotesBtn.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function AddNotesBtn() {
     return (
-        <div className='m-0 px-5 bg-white' style={{position:'sticky', top:60, zIndex:3}}>
+        <div className='m-0 px-5 bg-white' style={{position:'sticky', width:'100%', top:60, zIndex:3}}>
             <Link to='/addnotform' className='navbar-brand p-4 m-0'>
                 <div className='d-flex align-items-center justify-content-center p-1 ' style={{ background: 'white', borderRadius: '1rem', border: '2px solid #e5e5e5', cursor: 'pointer', }} >
                     <i className="fa-solid fa-plus fa-xl"></i>
